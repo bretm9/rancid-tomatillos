e4399a3c66360bf2573c4d996284ed1a70057349
Carlyle Douglas <cdouglascorp@gmail.com>
2020-10-06 16:05:58 -0600

    Add basic Header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Header() {
+  return (
+    <section className="header">
+      <h1>Rancid Tomatillos</h1>
+      <button>Login</button>
+    </section>
+  )
+}
+
+export default Header; 
