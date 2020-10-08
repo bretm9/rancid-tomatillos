ba457ab4a976a7adacc3085a784c8680c1230adb
Carlyle Douglas <cdouglascorp@gmail.com>
2020-10-08 17:59:10 -0600

    Add alt text for img

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -5,7 +5,7 @@ function Movies(props) {
   const moviesToDisplay = props.moviesList.map((movie, index) => {
     return (
       <section key={index} className="grid-item" id={movie.id.toString()}>
-        <img src={movie.poster_path}/>
+        <img alt="movie-thumbnail-img" src={movie.poster_path}/>
         <h3 className="movie-title">{movie.title}</h3>
         <h3 className="movie-rating">{movie.average_rating}</h3>
       </section>
