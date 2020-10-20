93a061015d7d7ed7cc30cde455705f09bbb86c0c
Bret Merritt <merrittbret9@gmail.com>
2020-10-20 14:09:59 -0600

    Header show login or logout depending on loginData state

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
+  if (props.loginData) {
+    return (
+      <section className="header">
+        <h1>Rancid Tomatillos</h1>
+        <h3>Hello, {props.loginData.user.name}</h3>
+        <NavLink to="/" className="nav">Homepage</NavLink>
+        <NavLink to="/login" className="nav">Logout</NavLink>
+      </section>
+    )
+  }
   return (
-    <section className="header">
+    <section>
       <h1>Rancid Tomatillos</h1>
-      <button>Login</button>
+      <h3>You are not logged in</h3>
+      <NavLink to="/" className="nav">Home</NavLink>
+      <NavLink to="/login" className="nav">Login Page</NavLink>
     </section>
   )
 }
